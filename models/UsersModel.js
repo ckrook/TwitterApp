37185fbe0875fece3036b0f643f9d1df7724d13f
Simon Sandahl <sandahl.simon@hotmail.se>
2022-02-25 15:27:24 +0100

    updated UsersModel

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -4,6 +4,8 @@ const usersSchema = new mongoose.Schema({
   username: { type: String, required: true },
   hashedPassword: { type: String, required: true },
   email: { type: String, required: true },
+  city: { type: String, required: true},
+  dateOfBirth: { type: Number, required: true },
   created: { type: Number, default: Date.now },
   role: { type: String, default: "User" },
   bio: String,
